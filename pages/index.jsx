c801106b04d8c49cd1a973571e838d4ec484df2b
Connor Simpson <cranavvo@gmail.com>
2021-02-02 12:32:47 +0000

    Remvoing blog posts for now

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,11 @@
 
 import React from 'react'
 import Header from '../components/website/header'
-import Blog from '../components/website/blog'
 
 export default function Home() {
   return (
     <>
       <Header title="I'm Connor, 👋" subtitle="I'm a software engineer who loves ☕" />
-      <Blog />
     </>
   )
 }
